90b8bbf71b3878b2ec98044f87482fbc3b7195a1
Ike Kottlowski <immanuel.kottlowski@gmail.com>
2015-03-31 13:05:43 -0400

    adding android UI elements: Tabbed, and Images

--- Android/QuaffOnBloomington/app/src/main/java/com/quaffon/team9/quaffonbloomington/MainActivity.java
+++ Android/QuaffOnBloomington/app/src/main/java/com/quaffon/team9/quaffonbloomington/MainActivity.java
@@ -1,43 +1,40 @@
 package com.quaffon.team9.quaffonbloomington;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+
+    private static final String Loading_TAG = "Loading Screen Timer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_loading_screen);
 
-        setContentView(R.layout.activity_main);
-    }
+        new CountDownTimer(2000, 1000){
+            public void onTick(long millisUntilFinished){
+                Log.v(Loading_TAG, "seconds remaining: " + millisUntilFinished / 1000);
+            }
+            public void onFinish() {
+                setContentView(R.layout.activity_verify);
+                findViewById(R.id.textView3).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setContentView(R.layout.activity_main);
+                    }
+                });
+                Log.v(Loading_TAG, "done!");
+            }
+        }.start();
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
